27db7d55227a2f18809330a8d5fd10d61715c90d
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-10-13 16:26:41 +0200

    add basic authenitcaiton with email

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
           className={cn(
             'min-h-screen font-sans antialiased grainy',
             inter.className)}>
+          <Navbar/>
           {children}
         </body>
     </html>
